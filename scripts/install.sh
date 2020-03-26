4ebf266ab8410bb61daf64fcc0ae49808ee70bbf
TheAvidDev <AvidDev@protonmail.com>
2020-03-26 16:12:13 -0400

    Remove all files

--- scripts/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cp ../dmoj/local_settings.py ../bridge/
-docker-compose build --no-cache
-docker-compose run --rm --entrypoint "python manage.py migrate" dmoj
-docker-compose run --rm --entrypoint "python manage.py loaddata language_small" dmoj
-docker-compose run --rm --entrypoint "python manage.py migrate" dmoj
-docker-compose run --rm --entrypoint "python manage.py loaddata navbar" dmoj
-docker-compose run --rm --entrypoint "python manage.py loaddata demo" dmoj
