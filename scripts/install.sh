5c7a1d62ea0a8a65442586ebcfee72083e4dd877
theaviddev <aviddev@protonmail.com>
2020-03-29 17:56:26 +0000

    Remove double call to migrate Fixed in DMOJ#1278

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 docker-compose run --rm --entrypoint "/bin/sh -c '\
         python manage.py migrate && \
-        python manage.py loaddata language_small && \
-        python manage.py migrate && \
         python manage.py loaddata navbar && \
         python manage.py loaddata demo'" dmoj
