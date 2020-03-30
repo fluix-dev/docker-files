6d226afc59ce57d04c01b9f3ebd3761d6f1457c3
theaviddev <aviddev@protonmail.com>
2020-03-30 03:28:23 +0000

    Add last loaddata to install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 docker-compose run --rm --entrypoint "/bin/sh -c '\
         python manage.py migrate && \
+        python manage.py loaddata language_small && \
         python manage.py loaddata navbar && \
         python manage.py loaddata demo'" dmoj
